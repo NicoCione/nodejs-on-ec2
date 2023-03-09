130035658d659bab6c4ac447ef0d483d93cde427
NicoCione <112523384+NicoCione@users.noreply.github.com>
2023-03-09 10:09:28 -0500

    firstTest

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  app.get("/cairn/logo", function(req, res) {
+    var requestSettings = {
+        url: 'https://cairn.edu/wp-content/uploads/2021/09/cairn_sealweb500.jpg',
+        method: 'GET',
+        encoding: null
+    };
+
+    request(requestSettings, function(error, response, body) {
+        res.set('Content-Type', 'image/png');
+        res.send(body);
+    });
+});
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
