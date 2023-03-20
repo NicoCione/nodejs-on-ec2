a9d430ceb6bd50dd20940c2e438f02ef19930974
NicoCione <112523384+NicoCione@users.noreply.github.com>
2023-03-20 00:34:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  app.get("/cairn/logo", function(req, res) {
-    var requestSettings = {
-        url: 'https://cairn.edu/wp-content/uploads/2021/09/cairn_sealweb500.jpg',
-        method: 'GET',
-        encoding: null
-    };
-
-    request(requestSettings, function(error, response, body) {
-        res.set('Content-Type', 'image/png');
-        res.send(body);
-    });
-});
+  res.write('<html><body><img src="https://cairn.edu/wp-content/uploads/2021/09/cairn_sealweb500.jpg" alt="CairnImage.jpeg" style="width:500px;height:500px;"></body></html>'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
